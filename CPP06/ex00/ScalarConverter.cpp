b582f72960b797243acdf4003907148a3d3a2f2f
Simon Tempels <stempels@shi-r12-p1.s19.be>
2025-12-12 11:35:35 +0100

    CPP06:push

--- CPP06/ex00/ScalarConverter.cpp
+++ CPP06/ex00/ScalarConverter.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.cpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stempels <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 10:05:00 by stempels          #+#    #+#             */
+/*   Updated: 2025/12/12 11:32:36 by stempels         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ScalarConverter.hpp"
 
 ScalarConverter::ScalarConverter() {
@@ -19,34 +31,78 @@ ScalarConverter&	ScalarConverter::operator=(ScalarConverter const& other) {
 	return (*this);
 }
 
-void	printChar(char c, bool is_possible = true) {
-	if ((0 < c && c < 32) || c == 127)
-		std::cout << "char: Non displayable" << std::endl;
-	else if (!is_possible || c < 0)
-		std::cout << "char: impossible" << std::endl;
-	else
-		std::cout << "char: '" << c << "'" << std::endl;
+template <typename From, typename To>
+int	is_convertible(From value) {
+	if (std::numeric_limits<From>::is_iec559) {
+		if (value != value)
+			return (2);
+	}	
+	if (std::numeric_limits<To>::is_iec559) {
+		if (value < -std::numeric_limits<To>::max()
+			|| std::numeric_limits<To>::max() < value)
+		return (1);
+	}
+	else if (value < std::numeric_limits<To>::min()
+		|| std::numeric_limits<To>::max() < value)
+		return (1);
+	return (0);
 }
 
-void	printInt(int i, bool is_possible = true) {
-	if (!is_possible)
-		std::cout << "int: impossible" << std::endl;
-	else
-		std::cout << "int: " << i << std::endl;
+void	printChar(char c, int action) {
+	switch (action) {
+		case (0):
+			if ((0 <= c && c < 32) || c == 127)
+				std::cout << "char: Non displayable" << std::endl;
+			else if (c < 0)
+				std::cout << "char: impossible" << std::endl;
+			else
+				std::cout << "char: '" << c << "'" << std::endl;
+			break ;
+		case (1):
+			std::cout << "char: overflow" << std::endl;
+			break ;
+		default:
+			std::cout << "char: impossible" << std::endl;
+	}
 }
 
-void	printFloat(float f, bool is_possible = true) {
-	if (!is_possible)
-		std::cout << "float: impossible" << std::endl;
-	else
-		std::cout << "float: " << std::setprecision(1) << std::fixed << f << "f" << std::endl;
+void	printInt(int c, int action) {
+	switch (action) {
+		case (0):
+				std::cout << "int: '" << c << "'" << std::endl;
+				break ;
+		case (1):
+			std::cout << "int: overflow" << std::endl;
+			break ;
+		default:
+			std::cout << "int: impossible" << std::endl;
+	}
 }
 
-void	printDouble(double d, bool is_possible = true) {
-	if (!is_possible)
-		std::cout << "double: impossible" << std::endl;
-	else
-		std::cout << "double: " << std::setprecision(1) << std::fixed << d << std::endl;
+void	printFloat(float c, int action) {
+	switch (action) {
+		case (0):
+				std::cout << std::fixed << std::setprecision(1) << "float: '" << c << "f'" << std::endl;
+				break ;
+		case (1):
+			std::cout << "float: overflow" << std::endl;
+			break ;
+		default:
+			std::cout << "float: impossible" << std::endl;
+	}
+}
+
+void	printDouble(double c, int action) {
+	switch (action) {
+		case (0):
+				std::cout << std::fixed << std::setprecision(1) << "double: '" << c << "'" << std::endl;
+				break ;
+		case (1):
+			std::cout << "double: overflow" << std::endl;
+			break ;
+		default:
+			std::cout << "double: impossible" << std::endl;
+	}
 }
 
 bool	isPseudoLiteral(const std::string& literal) {
@@ -92,12 +148,14 @@ bool	isValid(const std::string& literal) {
 void	toChar(const std::string& literal) {
 	char c;
 
-	c = literal[0];
-	printChar(c);
-	printInt(static_cast<int>(c));
-	printFloat(static_cast<float>(c));
-	printDouble(static_cast<double>(c));
+	c = literal.at(0);
+
+	printChar(c, 0);
+	printInt(static_cast<int>(c), is_convertible<char, int>(c));
+	printFloat(static_cast<float>(c), is_convertible<char, float>(c));
+	printDouble(static_cast<double>(c), is_convertible<char, double>(c));
 }
+
 
 void	toInt(const std::string& literal) {
 	std::stringstream ss;
@@ -105,86 +163,68 @@ void	toInt(const std::string& literal) {
 
 	ss << literal;
 	ss >> i;
-	printChar(static_cast<char>(i));
+
+	printChar(static_cast<char>(i), is_convertible<int, char>(i));
+
 	if (ss.fail())
-		printInt(0, false);
+		printInt(0, 1);
 	else
-		printInt(i);
-	printFloat(static_cast<float>(i));
-	printDouble(static_cast<double>(i));
+		printInt(i, 0);
+
+	printFloat(static_cast<float>(i), is_convertible<int, float>(i));
+	printDouble(static_cast<double>(i), is_convertible<int, double>(i));
+
 	ss.clear();
 }
 
-float	toFloat(std::string literal) {
-	// Permet de manipuler des chaînes de caractères comme si elles étaient
-	// des flux d'entrée ou de sortie.
-	std::stringstream ss1;
-	std::stringstream ss2;
+void	toFloat(const std::string& literal) {
+	std::stringstream ss;
 	float f;
-	int	i;
 
-	literal.erase(literal.size() - 1);
+	ss << literal;
+	ss >> f;
 
-	// Put the string into the stream
-	ss1 << literal;
-	ss2 << literal;
+	printChar(static_cast<char>(f), is_convertible<float, char>(f));
+	printInt(static_cast<int>(f), is_convertible<float, int>(f));
 
-	// Extract the float and the int from the stream
-	ss1 >> i;
-	ss2 >> f;
-
-	printChar(static_cast<char>(f));
-	if (ss1.fail())
-		printInt(0, false);
+	if (ss.fail())
+		printFloat(0, 1);
 	else
-		printInt(static_cast<int>(f));
-	if (ss2.fail())
-		printFloat(0, false);
-	else
-		printFloat(f);
-	printDouble(static_cast<double>(f));
+		printFloat(f, 0);
 
-	// Clear the stream
-	ss1.clear();
-	ss2.clear();
+	printDouble(static_cast<double>(f), is_convertible<float, double>(f));
 
-	return f;
+	ss.clear();
 }
-
 void	toDouble(const std::string& literal) {
-	std::stringstream ss1;
-	std::stringstream ss2;
-	int		i;
-	double	d;
+	std::stringstream ss;
+	double d;
 
-	ss1 << literal;
-	ss2 << literal;
+	ss << literal;
+	ss >> d;
 
-	ss1 >> i;
-	ss2 >> d;
-	printChar(static_cast<char>(d));
-	if (ss1.fail())
-		printInt(0, false);
+	 printChar(static_cast<char>(d), is_convertible<double, char>(d));
+	 printInt(static_cast<int>(d), is_convertible<double, int>(d));
+	 printFloat(static_cast<float>(d), is_convertible<double, float>(d));
+
+	if (ss.fail())
+		printDouble(0, 1);
 	else
-		printInt(static_cast<int>(d));
-	printFloat(static_cast<float>(d));
-	if (ss2.fail())
-		printDouble(0, false);
-	else
-		printDouble(d);
-	ss1.clear();
-	ss2.clear();
+		printDouble(d, 0);
+
+	ss.clear();
 }
 
 void	toPseudoLiteral(const std::string& literal) {
-	printChar('c', false);
-	printInt(0, false);
+	printChar(0, 2);
+	printInt(0, 2);
 	if	(literal == "-inff"
 		||	literal == "+inff"
 		|| literal == "nanf") {
 		std::cout << "float: " << literal << std::endl;
 		std::cout << "double: " << literal.substr(0, literal.length() - 1) << std::endl;
-	} else {
+	}
+	else {
 		std::cout << "float: " << literal << "f" << std::endl;
 		std::cout << "double: " << literal << std::endl;
 	}
